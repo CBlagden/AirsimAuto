9d5fd721d4d4c0abc2617022b80f0792d3feb047
Chase Blagden <chase.blagden@gmail.com>
2018-12-24 13:17:22 -0800

    implemented circular stack

---
+++ utils.py
@@ -0,0 +1,22 @@
+
+
+class CircularStack(list):
+
+    def __init__(self, size):
+        super().__init__()
+        self.size = size
+        self.stack = []
+
+    def update(self, item):
+        if len(self.stack) > self.size-1:
+            self.stack.pop(0)
+        self.stack.append(item)
+
+    def __getitem__(self, item):
+        return self.stack[item]
+
+    def __iter__(self):
+        return iter(self.stack)
+
+    def __len__(self):
+        return len(self.stack)
